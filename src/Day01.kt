6e0ebc6374793068f88ebbfba90619d0ebd62c2f
Piotr Piekarski <piotrek199507@gmail.com>
2025-12-01 23:32:10 +0100

    Day1 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,12 +1,9 @@
+import kotlin.math.abs
+
 private const val START = 50
 private const val MAX_EXCLUSIVE = 100
 
 fun main() {
-    fun String.moveIt() = first()
-        .let { direction -> this.drop(1).toInt() * if (direction == 'R') 1 else -1 }
-
-    fun Int.normalizePosition() = takeIf { it >= 0 } ?: (this + MAX_EXCLUSIVE)
-
     fun part1(input: List<String>): Int {
         var currentPosition = START
         return input
@@ -17,6 +14,37 @@ fun main() {
             }
     }
 
+    fun part2(input: List<String>): Int {
+        var currentPosition = START
+        var zeroesPassed = 0
+        for (line in input) {
+            val stepSize = line.moveIt()
+//            println("Pos $currentPosition, Step $line parsed as $stepSize")
+            val incrByFullRound = abs((stepSize + currentPosition) / 100)
+            zeroesPassed += incrByFullRound
+            if (movedBackOverZero(stepSize, currentPosition)) zeroesPassed++
+            currentPosition = getPosition(currentPosition, stepSize)
+            if (currentPosition == 0) { zeroesPassed++ }
+        }
+        return zeroesPassed
+    }
+
+
     val input = readInput("Day01")
-    part1(input).println()
+    val one = part1(input).also { it.println() } // 1182 OK
+    val two = part2(input).also { it.println() } // 8217 Not OK
 }
+
+fun String.moveIt() = first()
+    .let { direction -> this.drop(1).toInt() * if (direction == 'R') 1 else -1 }
+
+fun Int.normalizePosition() = takeIf { it >= 0 } ?: (this % 100 + MAX_EXCLUSIVE)
+
+fun getPosition(currentPosition: Int, stepSize: Int): Int {
+    val x =  (currentPosition + stepSize).normalizePosition() % MAX_EXCLUSIVE
+    require(x in 0..<MAX_EXCLUSIVE) { "X should be in range [0, $MAX_EXCLUSIVE)" }
+    return x
+}
+
+fun movedBackOverZero(stepSize: Int, currentPosition: Int): Boolean = stepSize + currentPosition < 0
+
