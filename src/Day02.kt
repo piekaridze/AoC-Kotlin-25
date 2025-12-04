7b2ae7e0ff32e640598960e8714114e786bcdb4b
Piotr Piekarski <piotrek199507@gmail.com>
2025-12-04 20:35:52 +0100

    measureTimePrint to Utils.kt

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,35 +1,14 @@
 import kotlin.math.log10
 import kotlin.math.pow
-import kotlin.time.measureTime
+import kotlin.math.sqrt
 
-
-fun measureTimePrint(measured: () -> Any) {
-    val value: Any
-    val time = measureTime { value = measured() }
-    println("Answer: $value (done in $time)")
-}
 
 fun main() {
-    fun part1(input: List<LongRange>): Long {
-        return input .fold(0L) { acc, range ->
-            val addition = range.filter { isTwoSameSequencesWithDividingSolution(it) }.sum()
-            return@fold acc + addition
-        }
-    }
-
-    fun part2(input: List<LongRange>): Long {
-        return input .fold(0L) { acc, range ->
-            val invalidIds = range.filter { isInvalidIdSameSequenceAtLeastTwice(it) }
-            val addition = invalidIds.sum()
-            return@fold acc + addition
-        }
-    }
-
     // Or read a large test input from the `src/Day02_test.txt` file:
     val testResult = part1(readLines("Day02_test"))
-    check(testResult == 1227775554L) { "Failed with $testResult"  }
+    check(testResult == 1227775554L) { "Failed with $testResult" }
     val testResult2 = part2(readLines("Day02_test"))
-    check(testResult2 == 4174379265L) { "Failed with $testResult"  }
+    check(testResult2 == 4174379265L) { "Failed with $testResult" }
 
     // Read the input from the `src/Day02.txt` file.
     val input = readLines("Day02")
@@ -41,13 +20,20 @@ private fun readLines(fileName: String): List<LongRange> = readInputByComa(fileN
     .map { it -> it.split("-").map { it.toLong() } }
     .map { LongRange(it[0], it[1]) }
 
-fun isTwoSameSequencesWithStringSolution(id: Long): Boolean {
+private fun part1(input: List<LongRange>): Long {
+    return input.fold(0L) { acc, range ->
+        val addition = range.filter { isTwoSameSequencesWithDividingSolution(it) }.sum()
+        return@fold acc + addition
+    }
+}
+
+private fun isTwoSameSequencesWithStringSolution(id: Long): Boolean {
     val asString = id.toString()
     return asString.length % 2 == 0 &&
             asString.take(asString.length / 2) == asString.substring(asString.length / 2)
 }
 
-fun isTwoSameSequencesWithDividingSolution(id: Long): Boolean {
+private fun isTwoSameSequencesWithDividingSolution(id: Long): Boolean {
     val digits = if (id == 0L) 1 else (log10(id.toDouble()).toInt() + 1)
     if (digits % 2 != 0) return false
     val half = digits / 2
@@ -57,12 +43,20 @@ fun isTwoSameSequencesWithDividingSolution(id: Long): Boolean {
 
 // part 2
 
+private fun part2(input: List<LongRange>): Long {
+    return input.fold(0L) { acc, range ->
+        val invalidIds = range.filter { isInvalidIdSameSequenceAtLeastTwice(it) }
+        val addition = invalidIds.sum()
+        return@fold acc + addition
+    }
+}
+
 /**
  * an ID is invalid if it is made only of some sequence of digits repeated at least twice.
  * So, 12341234 (1234 two times), 123123123 (123 three times),
  * 1212121212 (12 five times), and 1111111 (1 seven times) are all invalid IDs.
  */
-fun isInvalidIdSameSequenceAtLeastTwice(id: Long): Boolean {
+private fun isInvalidIdSameSequenceAtLeastTwice(id: Long): Boolean {
     //  1111111 -> {[1,1,1,1,1,1,1]}, 123123 -> {[1,2,3,1,2,3], [12,31,23], [123,123]} - find if any array has all elements the same
     if (id < 10) return false
     val numberString = id.toString()
@@ -72,16 +66,16 @@ fun isInvalidIdSameSequenceAtLeastTwice(id: Long): Boolean {
 
 private fun String.tokenizedWithSizeAreAllTheSame(divisor: Int) = this.chunked(divisor).toSet().size == 1
 
-val divisors = HashMap<Int, List<Int>>()
+private val divisors = HashMap<Int, List<Int>>()
 
-fun getDivisors(number: Int): List<Int> {
+private fun getDivisors(number: Int): List<Int> {
     return divisors.computeIfAbsent(number) { computeDivisors(it) }
 }
 
-fun computeDivisors(number: Int): List<Int> {
+private fun computeDivisors(number: Int): List<Int> {
     val divisors = mutableSetOf<Int>()
     divisors.add(1)
-    for (i in 2..kotlin.math.sqrt(number.toDouble()).toInt()) {
+    for (i in 2..sqrt(number.toDouble()).toInt()) {
         if (number % i == 0) {
             divisors.add(i)
             divisors.add(number / i)
@@ -89,4 +83,3 @@ fun computeDivisors(number: Int): List<Int> {
     }
     return divisors.toList()
 }
-
--- src/Utils.kt
+++ src/Utils.kt
@@ -2,6 +2,7 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
+import kotlin.time.measureTime
 
 /**
  * Reads lines from the given input txt file.
@@ -20,3 +21,9 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun measureTimePrint(measured: () -> Any) {
+    val value: Any
+    val time = measureTime { value = measured() }
+    println("Answer: $value (done in $time)")
+}
