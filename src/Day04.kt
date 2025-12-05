d9e843ebe2600bd8800c252fffcacede3c0d6f3c
Piotr Piekarski <piotrek199507@gmail.com>
2025-12-05 20:53:34 +0100

    Day 04 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -6,35 +6,18 @@ fun main() {
     val testResult = part1(readInputAs2D("Day04_test"))
     check(testResult == 13) { "Failed with $testResult" }
 
-//    val testResult2 = part2(readInput("Day04_test"))
-//    check(testResult2 == 0) { "Failed with $testResult2" }
+    val testResult2 = part2(readInputAs2D("Day04_test"))
+    check(testResult2 == 43) { "Failed with $testResult2" }
 
     val input = readInputAs2D("Day04")
-    measureTimePrint { part1(input) }
-//    measureTimePrint { part2(input) }
+    measureTimePrint { part1(input) } // 1486
+    measureTimePrint { part2(input) } // 9024
 }
 
-fun readInputAs2D(name: String): List<List<Char>> {
-    return readInput(name).map { it.toCharArray().toList() }
-}
+fun readInputAs2D(name: String): List<MutableList<Char>> = readInput(name)
+    .map { it.toCharArray().toMutableList() }
 
-private fun part1(cells: List<List<Char>>): Int {
-    var canLift = 0
-    val context = MapContext(cells.size - 1, cells[0].size - 1)
-    cells.indices.forEach { rowN ->
-        cells[rowN].indices.forEach { colN ->
-            val current = cells[rowN][colN]
-            if (current.isPaper()) {
-                validNeighboursCoordinates(rowN, colN, context)
-                    .map { cells[it.first][it.second] }
-                    .count { it.isPaper() }
-                    .takeIf { paperNeighboursCount -> paperNeighboursCount < 4 }
-                    ?.run { canLift++ }
-            }
-        }
-    }
-    return canLift
-}
+private fun part1(cells: List<List<Char>>): Int = processOnce(cells)
 
 private fun validNeighboursCoordinates(rowN: Int, colN: Int, context: MapContext): List<Pair<Int, Int>> = directions
     .map { it.first + rowN to it.second + colN }
@@ -51,4 +34,43 @@ private fun Char.isPaper() = this == '@'
 data class MapContext(val maxRowNumber: Int, val maxColNumber: Int)
 
 
-private fun part2(input: List<List<Char>>): Int = TODO()
+private fun part2(cells: List<MutableList<Char>>): Int {
+    var liftedThisTime: Int
+    var accumulated = 0
+    do {
+        toRemove.forEach { cells.removePaper(it)}
+        toRemove.clear()
+        liftedThisTime = processOnce(cells, modify = true)
+        accumulated += liftedThisTime
+        println("lifted $liftedThisTime")
+    } while (liftedThisTime > 0)
+    return accumulated
+}
+
+private fun List<MutableList<Char>>.removePaper(rowToCell: Pair<Int, Int>) {
+    require(this[rowToCell.first][rowToCell.second].isPaper()) {"Removing paper from $rowToCell, but its not pape" }
+    this[rowToCell.first][rowToCell.second] = '.'
+}
+
+private fun processOnce(cells: List<List<Char>>, modify: Boolean = false): Int {
+    var canLift = 0
+    val context = MapContext(cells.size - 1, cells[0].size - 1)
+    cells.indices.forEach { rowN ->
+        cells[rowN].indices.forEach { colN ->
+            val current = cells[rowN][colN]
+            if (current.isPaper()) {
+                validNeighboursCoordinates(rowN, colN, context)
+                    .map { cells[it.first][it.second] }
+                    .count { it.isPaper() }
+                    .takeIf { paperNeighboursCount -> paperNeighboursCount < 4 }
+                    ?.run { canLift++ }
+                    ?.takeIf { modify }?.run { (rowN to colN).markForRemoval() }
+            }
+        }
+    }
+    return canLift
+}
+
+private fun Pair<Int, Int>.markForRemoval() = toRemove.add(this)
+
+private val toRemove = mutableSetOf<Pair<Int, Int>>()
